0493cc8f09bebac390186367763ea062b6748747
theweird0ne <heyamlan19@gmail.com>
2024-02-08 13:53:39 +0530

    feat:hero component completed

---
+++ client/src/components/Hero/Hero.jsx
@@ -0,0 +1,64 @@
+import { BsFillRocketTakeoffFill } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { MdMailOutline, MdOutlineEventAvailable } from "react-icons/md";
+import "./Hero.css";
+export const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero__banner">
+        <img src="/banner.png" alt="waves" />
+      </div>
+      <div className="hero__body">
+        <div className="profile__heading">
+          <img src="/profile.jpg" alt="Me" className="profile__pic" />
+          <a
+            href="https://www.linkedin.com/in/amlanxyz"
+            className="hireme__btn"
+          >
+            Hire Me <BsFillRocketTakeoffFill />
+          </a>
+        </div>
+        <div className="profile__content">
+          <h1>Amlanjyoti Saikia</h1>
+          <small>Crafting user-friendly web experiences.</small>
+          <p>
+            Hi, I'm a Full-Stack web developer specializing in MERN stack and a
+            Computer Science & Engineering grad.
+          </p>
+          <ul className="profile__links">
+            <li className="disable">
+              <MdOutlineEventAvailable className="disable__icon" /> Available
+            </li>
+            <li className="disable">
+              <FaGithub className="disable__icon" />
+              <a href="https://github.com/amlan-xyz" className="disable">
+                Github
+              </a>
+            </li>
+            <li className="disable">
+              <MdMailOutline className="disable__icon" />
+              <a href="mailto:heyamlan19@gmail.com" className="disable">
+                heyamlan19@gmail.com
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="profile__skills">
+          <h3>My skills</h3>
+          <ul className="profile__skills-ul">
+            <li className="profile__skills-li">React</li>
+            <li className="profile__skills-li">Redux</li>
+            <li className="profile__skills-li">JavaScript</li>
+            <li className="profile__skills-li">MongoDB</li>
+            <li className="profile__skills-li">Node.js</li>
+            <li className="profile__skills-li">Express.js</li>
+            <li className="profile__skills-li">HTML5</li>
+            <li className="profile__skills-li">CSS3</li>
+            <li className="profile__skills-li">Typescript</li>
+            <li className="profile__skills-li">Git</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
